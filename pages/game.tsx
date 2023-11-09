731a6ffab570b312840cf4de964cbfb99d358985
abouthugo <hugo@perdomo.dev>
2023-11-09 15:55:52 -0500

    game page select component to pick a game to test

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,7 +1,32 @@
 import GlassSteppingStones from '../components/Game/GlassSteppingStones'
 import GreenLightRedLight from '../components/Game/GreenLightRedLight'
 import NumberGuesser from '../components/Game/NumberGuesser'
+import { useState } from 'react'
 
 export default function GamePage() {
-  return <GlassSteppingStones />
+  const [selectedGame, setSelectedGame] = useState(null)
+
+  const handleGameSelection = (event) => {
+    setSelectedGame(event.target.value)
+  }
+
+  return (
+    <>
+      <select
+        onChange={handleGameSelection}
+        className="select select-bordered w-full max-w-xs"
+        defaultValue="none"
+      >
+        <option disabled value="none">
+          Select a game to test
+        </option>
+        <option value="GreenLightRedLight">Green Light Red Light</option>
+        <option value="NumberGuesser">Number Guesser</option>
+        <option value="GlassSteppingStones">Glass Stepping Stones</option>
+      </select>
+      {selectedGame === 'GreenLightRedLight' && <GreenLightRedLight />}
+      {selectedGame === 'NumberGuesser' && <NumberGuesser />}
+      {selectedGame === 'GlassSteppingStones' && <GlassSteppingStones />}
+    </>
+  )
 }
